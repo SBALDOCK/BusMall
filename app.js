65e1c87a03f9060744a7a8b9394d983e8c75a805
SBALDOCK <stephenbaldock78@gmail.com>
2020-05-11 14:43:44 -0700

    three photos are showing

--- app.js
+++ app.js
@@ -4,36 +4,36 @@ var parentElement = document.getElementById('busMallParent');
 
 var allItems = [];
 
-function Catalog(url, alt, title){
+function Catalog(url, alt, name){
   this.filePath = url;
   this.alt = alt;
-  this.title = title;
-  this.votes = votes;
+  this.name = name;
+  this.votes = 0;
   this.views = 0;
-  this.percentage = percentage;
+  // this.percentage = percentage;
   allItems.push(this);
 }
 
-new Catalog('img/bag.jpg', 'bag', 'bag');
-new Catalog('img/banana.jpg', 'banana', 'banana');
-new Catalog('img/bathroom.jpg', 'bathroom', 'bathroom');
-new Catalog('img/boots.jpg', 'boots', 'boots');
-new Catalog('img/breakfast.jpg', 'breakfast', 'breakfast');
-new Catalog('img/bubblegum.jpg', 'bubblegum', 'bubblegum');
-new Catalog('img/chair.jpg', 'chair', 'chair');
-new Catalog('img/cthulhu.jpg', 'cthulhu', 'cthulhu');
-new Catalog('img/dog-duck.jpg', 'dog-duck', 'dog-duck');
-new Catalog('img/dragon.jpg', 'dragon', 'dragon');
-new Catalog('img/pen.jpg', 'pen', 'pen');
-new Catalog('img/pet-sweep.jpg', 'pet-sweep', 'pet-sweep');
-new Catalog('img/scissors.jpg', 'scissors', 'scissors');
-new Catalog('img/shark.jpg', 'shark', 'shark');
-new Catalog('img/sweep.png', 'sweep', 'sweep');
-new Catalog('img/tauntaun.jpg', 'tauntaun', 'tauntaun');
-new Catalog('img/unicorn.jpg', 'unicorn', 'unicorn');
-new Catalog('img/usb.gif', 'usb', 'usb');
-new Catalog('img/water-can.jpg', 'water-can', 'water-can');
-new Catalog('img/wine-glass.jpg', 'wine-glass', 'wine-glass');
+new Catalog('pics/bag.jpg', 'bag', 'Bag');
+new Catalog('pics/banana.jpg', 'banana', 'Banana');
+new Catalog('pics/bathroom.jpg', 'bathroom', 'Bathroom');
+new Catalog('pics/boots.jpg', 'boots', 'Boots');
+new Catalog('pics/breakfast.jpg', 'breakfast', 'Breakfast');
+new Catalog('pics/bubblegum.jpg', 'bubblegum', 'Bubblegum');
+new Catalog('pics/chair.jpg', 'chair', 'Chair');
+new Catalog('pics/cthulhu.jpg', 'cthulhu', 'Cthulhu');
+new Catalog('pics/dog-duck.jpg', 'dog-duck', 'Dog-duck');
+new Catalog('pics/dragon.jpg', 'dragon', 'Dragon');
+new Catalog('pics/pen.jpg', 'pen', 'Pen');
+new Catalog('pics/pet-sweep.jpg', 'pet-sweep', 'Pet-sweep');
+new Catalog('pics/scissors.jpg', 'scissors', 'Scissors');
+new Catalog('pics/shark.jpg', 'shark', 'Shark');
+new Catalog('pics/sweep.png', 'sweep', 'Sweep');
+new Catalog('pics/tauntaun.jpg', 'tauntaun', 'Tauntaun');
+new Catalog('pics/unicorn.jpg', 'unicorn', 'Unicorn');
+new Catalog('pics/usb.gif', 'usb', 'Usb');
+new Catalog('pics/water-can.jpg', 'water-can', 'Water-can');
+new Catalog('pics/wine-glass.jpg', 'wine-glass', 'Wine-glass');
 
 Catalog.prototype.addImage = function(){
   //create an element - img
@@ -43,8 +43,8 @@ Catalog.prototype.addImage = function(){
   //fill in alt
   imageElement.setAttribute('alt', this.alt);
   // fill in title
-  imageElement.setAttribute('title', this.title);
-  //append to parent element (parent)
+  imageElement.setAttribute('title', this.name);
+  //append to parent element (parentElement)
   parentElement.appendChild(imageElement);
 };
 
@@ -87,11 +87,11 @@ getRandomImage();
 //}
 
 //set up an event listener
-parent.addEventListener('click', function(){
-  var itemThatWasClickedOn = event.target.title;
+parentElement.addEventListener('click', function(){
+  var itemThatWasClickedOn = event.target.name;
 
   for(var i = 0; i<allItems.length; i++){
-    if(itemThatWasClickedOn === allItems[i].title){
+    if(itemThatWasClickedOn === allItems[i].name){
       allItems[i].votes++;
     }
   }
